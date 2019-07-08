8cba0dcf9e8aaff8c1e774818d71fbc8d6647df9
Victoria-Hodder <morethangrammar@gmail.com>
2019-07-08 21:00:53 +0200

    created snippets file for drafts

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -7,7 +7,6 @@ import os
 app = Flask(__name__)
 SECRET_KEY = os.urandom(16)
 app.config['SECRET_KEY'] = SECRET_KEY
-        #app.config['SECRET KEY'] = '21f61b7d2435cceca0c077d9e94125ad'
 
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
