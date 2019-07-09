f744241f6c9b942a78c3bd1e57d8b3d51353a7da
Victoria-Hodder <morethangrammar@gmail.com>
2019-07-09 15:35:53 +0200

    created a snippets file for draft code and began to customise the design

--- flaskblog/__init__.py
+++ flaskblog/__init__.py
@@ -1,5 +1,5 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy #added this in models video
+from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 import os
